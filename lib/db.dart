2c809722f804b33b48de072044ebe4b5cfbd598c
fonkamloic <fonkamloic@gmail.com>
2019-11-22 02:38:47 +0100

    statict_name_and_links

---
+++ lib/db.dart
@@ -0,0 +1,44 @@
+List<String> images = [
+  'images/FLoic.jpg',
+  'images/loic_black.jpg',
+  'images/loic_hoody.jpg',
+  'images/leonel.png',
+  'images/FLoic.jpg',
+  'images/loic_black.jpg',
+  'images/loic_hoody.jpg',
+  'images/leonel.png',
+  'images/FLoic.jpg',
+  'images/loic_black.jpg',
+  'images/loic_hoody.jpg',
+  'images/leonel.png',
+];
+
+List<String> names = [
+  'Loic',
+  'fred',
+  'Fonkam',
+  'Leonel',
+  'Loic',
+  'fred',
+  'Fonkam',
+  'Leonel',
+  'Loic',
+  'fred',
+  'Fonkam',
+  'Leonel',
+];
+
+List<String> text = [
+  'hello',
+  "How are you?",
+  "I love you!",
+  "bea",
+  'hello',
+  "How are you?",
+  "I love you!",
+  "bea",
+  'hello',
+  "How are you?",
+  "I love you!",
+  "bea",
+];
